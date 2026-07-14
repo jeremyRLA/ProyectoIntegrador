42d905ebba77192b7aa7f7cdd0081472008f3acc
Jeremy <amyluc3p@gmail.com>
2026-07-14 10:02:47 -0500

    feat: Partidos Controller

---
+++ Controllers/PartidosController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UTNGolCoinApi.Data;
+using UTNGolCoinApi.Models;
+
+
+namespace UTNGolCoinApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartidosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PartidosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("crear")]
+        public IActionResult CrearPartido([FromBody] Partido partido)
+        {
+            _context.Partidos.Add(partido);
+            _context.SaveChanges();
+
+            return Ok( new
+                { exito = true,
+                  mensaje = "Partido creado exitosamente",      
+                  partido = partido
+            });
+        }
+        [HttpGet("disponibles")]
+        public IActionResult ObtenerPartidosDisponibles()
+        {
+            var partidos = _context.Partidos
+            .Where(p => p.Estado=="PROGRAMADO")
+            .OrderBy(p => p.FechaPartido)
+            .ToList();
+
+            return Ok(partidos);
+        }
+    }
+}
--- Models/Partido.cs
+++ Models/Partido.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public required string Codigo { get; set; }
-        public required string EquipoLocal { get; set; }
-        public required string EquipoVisitante { get; set; }
+        public required string Equipo1 { get; set; }
+        public required string Equipo2 { get; set; }
         public DateTime FechaPartido { get; set; }
         public decimal CuotaLocal { get; set; }
         public decimal CuotaEmpate { get; set; }
